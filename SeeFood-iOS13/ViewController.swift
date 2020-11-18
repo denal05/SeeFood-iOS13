232c38089db8369f76cf79fdc9e54152760c9d01
Denis Aleksandrov <denis.aleksandrov@codewell.mk>
2020-11-18 09:02:00 +0100

    Added func detect(image: CIImage) which is being called in func imagePickerController(), printing the VN classification observation to the debug log.

--- SeeFood-iOS13/ViewController.swift
+++ SeeFood-iOS13/ViewController.swift
@@ -27,9 +27,33 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userPickedImage
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError(#function + ": Could not convert the user-picked UI image to CI image!")
+            }
+            detect(image: ciImage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        guard let inceptionv3Model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError(#function + ": Loading CoreML model \"Inceptionv3\" into Vision failed!")
         }
         
-        imagePicker.dismiss(animated: true, completion: nil)
+        let request = VNCoreMLRequest(model: inceptionv3Model) { (vnCoreMLRequest, error) in
+            guard let results = vnCoreMLRequest.results as? [VNClassificationObservation] else {
+                fatalError(#function + ": Model failed to process image!")
+            }
+            print(#function + ": Results: \(results)")
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(#function + ": Error: \(error)")
+        }
     }
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
